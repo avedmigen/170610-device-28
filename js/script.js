e1c4b785eac6e6e2ab39fd3e07211a370cda7ad5
Dmitry Averin <avedmigen@gmail.com>
2020-06-30 11:48:19 +0300

    Добавляет проверку заполнения полей формы на js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const subMenu = document.querySelector(".submenu")
 const mapImagePreview = document.querySelector(".map-image-preview")
 const modalMap = document.querySelector(".modal-map")
 const modalMapCloseBtn = document.querySelector(".modal-map .modal-close")
-const modalContactForm = document.querySelector(".modal-contact-form")
+const modalContactFormPopup = document.querySelector(".modal-contact-form")
 const modalContactFormLink = document.querySelector(".contact-us a.button")
 const modalContactFormCloseBtn = document.querySelector(".modal-contact-form .modal-close")
+const modalContactForm = modalContactFormPopup.querySelector("form")
 const name = document.querySelector("[name = name]")
+const email = document.querySelector("[name = email]")
+const message = document.querySelector("[name = message]")
 
 submenuToggleButton.addEventListener("click", function (evt) {
   subMenu.classList.toggle("visually-hidden")
@@ -24,13 +27,20 @@ modalMapCloseBtn.addEventListener("click", function (evt) {
 
 modalContactFormLink.addEventListener("click", function (evt) {
   evt.preventDefault()
-  modalContactForm.classList.remove("visually-hidden")
+  modalContactFormPopup.classList.remove("visually-hidden")
   name.focus()
 })
 
 modalContactFormCloseBtn.addEventListener("click", function (evt) {
   evt.preventDefault()
-  modalContactForm.classList.add("visually-hidden")
+  modalContactFormPopup.classList.add("visually-hidden")
+})
+
+modalContactForm.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value || !message.value) {
+    evt.preventDefault()
+    console.log("Заполните все поля формы")
+  }
 })
 
 
